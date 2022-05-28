f77811cfc90fb80af470f178c2f74ba5dc168c91
Amrit Atwal <amritatwal04@gmail.com>
2022-05-28 21:24:44 +0100

    Folder structure reconfigured and navbar component added

---
+++ src/components/Navigation/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './Navbar.module.css';
+
+const Navbar = () => {
+    return (
+        <navbar className={css.navbar}>
+            Bluffball
+        </navbar>
+    )
+}
+
+export default Navbar
